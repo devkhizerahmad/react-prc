a9e80d7f601142d6a7f9d35f96aa5662a91b98b5
dev_khizer <devkhizerahmad@gmail.com>
2026-01-26 13:53:42 +0500

    add media

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./styles/variables.css";
 import "./styles/globals.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Cards from "./pages/Cards";
 function App() {
   return (
-    <Router>
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/card" element={<Cards />} />
       </Routes>
-    </Router>
+    
   );
 }
 
