7d8e31d01e0cad8ce4d15534aa4035513ec66a24
dev_khizer <devkhizerahmad@gmail.com>
2026-01-28 20:47:20 +0500

    upadated

--- frontend/src/compenents/sections/CardGrid.jsx
+++ frontend/src/compenents/sections/CardGrid.jsx
@@ -10,13 +10,13 @@ function CardGrid() {
       <Card title="Card 3" description="This is card 3" />
       <Card title="Card 4" description="This is card 4" />
       <Card title="Card 5" description="This is card 5" />
-      <Card title="Card 6" description="This is card 6" />
+      <Card title="Card 6" description/>
       <Card title="Card 7" description="This is card 7" />
       <Card title="Card 8" description="This is card 8" />
       <Card title="Card 9" description="This is card 9" />
-      <Card title="Card 10" description="This is card 10" />
-      <Card title="Card 11" description="This is card 11" />
-      <Card title="Card 12" description="This is card 12" />
+      <Card title="Card 10" />
+      <Card title="Card 11" />
+      <Card title="Card 12" />
     </div>
   );
 }
