8a990928fa73c85e6460517c1a8c828e549a8f15
dev_khizer <devkhizerahmad@gmail.com>
2026-01-28 20:44:36 +0500

    updated

--- frontend/src/compenents/sections/CardGrid.jsx
+++ frontend/src/compenents/sections/CardGrid.jsx
@@ -5,16 +5,17 @@ import '../../styles/sections.css'
 function CardGrid() {
   return (
     <div className="card-grid">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card title="Card 1" />
+      <Card title="Card 2" />
+      <Card title="Card 3" />
+      <Card title="Card 4" />
       <Card />
       <Card />
       <Card />
       <Card />
       <Card />
       <Card/>
+      <Card />
       <Card />
     </div>
   );
