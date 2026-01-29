ca010d75602b1279f91227a26f5fe32ce79433ae
dev_khizer <devkhizerahmad@gmail.com>
2026-01-29 21:33:10 +0500

    updated

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -13,7 +13,7 @@ function Signup() {
   const [signup, { isLoading, isError, error }] = useSignupMutation();
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e();
 
     try {
       const res = await signup({ name, email, password }).unwrap();
